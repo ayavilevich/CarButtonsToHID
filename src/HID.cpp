d4a83ca8dd1fbfe6cf3e9b9a5fc963d57815a2f5
Arik Yavilevich <arik@yavilevich.com>
2020-06-11 08:47:22 +0300

    macro implementation, wip

--- src/HID.cpp
+++ src/HID.cpp
@@ -3,7 +3,7 @@
 
 #include "HID.hpp"
 
-#define MAX_DIGITIZER 16383
+#define MAX_DIGITIZER 4095
 
 const uint8_t reportDescription[] = {HID_CONSUMER_REPORT_DESCRIPTOR(), HID_KEYBOARD_REPORT_DESCRIPTOR(),
 									 // HID_ABS_MOUSE_REPORT_DESCRIPTOR() // HID_MOUSE_REPORT_ID
@@ -99,6 +99,7 @@ void HID::DoTouchSwipeLeft() {
 	Mouse.move(MAX_DIGITIZER * 20 / 100, MAX_DIGITIZER / 2);
 	delay(50);
 	Mouse.release();
+	delay(1000); // let it finish the swipe animation
 }
 
 void HID::DoTouchSwipeRight() {
@@ -111,27 +112,35 @@ void HID::DoPowerConfigMenu() {
 	DoConsHome();
 	delay(50);
 	DoConsHome();
-	delay(50);
+	delay(200);
 	// press settings icon
 	Mouse.move(MAX_DIGITIZER * 155 / 175, MAX_DIGITIZER * 80 / 95);
 	Mouse.press();
 	delay(50);
 	Mouse.release();
 	delay(150);
-	// 5x down
-	for (int i = 0; i < 5; i++) {
+	Mouse.move(1, 1);
+	// TODO, should I be in range to move this away?
+	// get to top
+	Keyboard.press(KEY_UP_ARROW);
+	delay(2000);
+	Keyboard.release(KEY_UP_ARROW);
+	delay(100);
+	// 9x down
+	for (int i = 0; i < 9; i++) {
 		DoKbDownArrow();
-		delay(50);
+		delay(100);
 	}
 	Keyboard.press(KEY_RETURN);
 	Keyboard.release(KEY_RETURN);
-	delay(50);
+	delay(100);
 	// select MacroDroid
 	DoKbDownArrow();
 	delay(50);
 	Keyboard.press(KEY_RETURN);
 	Keyboard.release(KEY_RETURN);
 	delay(50);
+	/*
 	// toggle
 	Keyboard.press(' ');
 	Keyboard.release(' ');
@@ -160,6 +169,7 @@ void HID::DoPowerConfigMenu() {
 	delay(50);
 	Mouse.release();
 	delay(150);
+	*/
 }
 
 void HID::DoDvrCleanup() {
@@ -175,14 +185,20 @@ void HID::DoDvrCleanup() {
 	Mouse.press();
 	delay(50);
 	Mouse.release();
-	delay(150);
+	delay(1000);
 	// press "run now"
+	Mouse.move(MAX_DIGITIZER / 2, MAX_DIGITIZER * 88 / 95);
+	Mouse.press();
+	delay(50);
+	Mouse.release();
+	/*
 	Keyboard.press(KEY_TAB);
 	Keyboard.release(KEY_TAB);
-	delay(50);
+	delay(200);
 	Keyboard.press(KEY_RETURN);
 	Keyboard.release(KEY_RETURN);
 	delay(50);
+	*/
 }
 
 void HID::DoDvrToggle() {
@@ -196,7 +212,7 @@ void HID::DoDvrToggle() {
 	Mouse.press();
 	delay(50);
 	Mouse.release();
-	delay(150);
+	delay(2000);
 	// press recorder icon
 	Mouse.move(MAX_DIGITIZER * 65 / 175, MAX_DIGITIZER * 90 / 95);
 	Mouse.press();
@@ -215,7 +231,7 @@ void HID::DoBtMusicToggle() {
 	Mouse.press();
 	delay(50);
 	Mouse.release();
-	delay(150);
+	delay(1000);
 	// press play icon
 	Mouse.move(MAX_DIGITIZER / 2, MAX_DIGITIZER * 90 / 95);
 	Mouse.press();
@@ -226,38 +242,38 @@ void HID::DoBtMusicToggle() {
 void HID::DoEmailToSelf() {
 	Consumer.press(0x18A); // run email app
 	Consumer.release();
-	delay(150);
+	delay(3000);
 	// press compose icon
 	Mouse.move(MAX_DIGITIZER * 165 / 175, MAX_DIGITIZER * 85 / 95);
 	Mouse.press();
 	delay(50);
 	Mouse.release();
-	delay(150);
+	delay(2000);
 	// type "to"
 	Keyboard.press('a');
 	Keyboard.release('a');
-	delay(50);
+	delay(100);
 	Keyboard.press('r');
 	Keyboard.release('r');
-	delay(50);
+	delay(100);
 	Keyboard.press('i');
 	Keyboard.release('i');
-	delay(50);
+	delay(100);
 	Keyboard.press('k');
 	Keyboard.release('k');
-	delay(50);
+	delay(500);
 	DoKbDownArrow();
-	delay(50);
+	delay(200);
 	Keyboard.press(KEY_RETURN);
 	Keyboard.release(KEY_RETURN);
-	delay(50);
+	delay(200);
 	// move to "subject"
 	Keyboard.press(KEY_TAB);
 	Keyboard.release(KEY_TAB);
-	delay(50);
+	delay(100);
 	Keyboard.press(KEY_TAB);
 	Keyboard.release(KEY_TAB);
-	delay(50);
+	delay(100);
 }
 
 void HID::DoRadio() {
@@ -274,5 +290,17 @@ void HID::DoRadio() {
 }
 
 void HID::DoNavigation() {
-	// TODO
+	// get home
+	DoConsHome();
+	delay(50);
+	DoConsHome();
+	delay(50);
+	// change to second panel
+	DoTouchSwipeLeft();
+	// press old file cleanup icon
+	Mouse.move(MAX_DIGITIZER * 125 / 175, MAX_DIGITIZER * 30 / 95);
+	Mouse.press();
+	delay(50);
+	Mouse.release();
+	delay(100);
 }
